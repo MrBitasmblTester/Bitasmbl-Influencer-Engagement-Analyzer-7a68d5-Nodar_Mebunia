873e56ec6769bed9b0adab1c4f85b26a54c197e3
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-19 11:34:28 +0400

    Add InfluencerMetrics.java

---
+++ src/main/java/com/example/influencer/model/InfluencerMetrics.java
@@ -0,0 +1,8 @@
+package com.example.influencer.model;
+
+public class InfluencerMetrics {
+  public String id;
+  public int postCount;
+  public double engagementRate;
+  public InfluencerMetrics(String id,int postCount,double engagementRate){this.id=id;this.postCount=postCount;this.engagementRate=engagementRate;}
+}
